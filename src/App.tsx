ea7d789cc2dd58f522ff876532ba418fc90f7420
snowkidind <keny@eastcoastbands.com>
2023-10-21 20:48:58 +0700

    hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Index from './components/Index.tsx'
 import Mint from './components/Mint.tsx'
 import Sets from './components/Sets.tsx'
